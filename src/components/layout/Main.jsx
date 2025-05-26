3f68efc904dbe6840dca918f6cdbb752b401da35
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-05-26 16:29:04 +0200

    add form and submit button

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 export default function Main() {
-  const itemsList = [
+  const [itemsList, setItemsList] = useState([
     "titoloArticolo1",
     "titoloArticolo2",
     "titoloArticolo3",
     "titoloArticolo4",
-  ];
+  ]);
+  const [newItem, setNewItem] = useState("");
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const newItemsList = [...itemsList, newItem];
+    setItemsList(newItemsList);
+  };
   return (
     <main>
       <div className="container">
+        <form onSubmit={handleFormSubmit} className="d-flex gap-2">
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            className="form-control grow-1"
+            type="text"
+          />
+          <button className="btn btn-primary">Aggiungi</button>
+        </form>
         <ul>
           {itemsList.map((item, index) => (
             <li key={index}>{item}</li>
